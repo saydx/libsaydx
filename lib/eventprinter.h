f0d8aa41ce5f706d26bfed73d9f0906ddbd6cca0
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2020-01-22 15:17:41 +0000

    Added headers referring to the licence
    Used the https://github.com/google/addlicense tool.

--- lib/eventprinter.h
+++ lib/eventprinter.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020 Bálint Aradi, Universität Bremen. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #ifndef SAYDX_EVENTPRINTER_H
 #define SAYDX_EVENTPRINTER_H
 
--- lib/treebuilder.h
+++ lib/treebuilder.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020 Bálint Aradi, Universität Bremen. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 #ifndef SAYDX_TREEBUILDER_H
 #define SAYDX_TREEBUILDER_H
 
